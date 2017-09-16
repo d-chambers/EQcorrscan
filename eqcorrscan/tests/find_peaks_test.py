c645ada5cb757e422bc71d3486c3614486b2f755
derrick <djachambeador@gmail.com>
2017-09-16 10:46:08 -0700

    refactored find peak tests

--- eqcorrscan/tests/find_peaks_test.py
+++ eqcorrscan/tests/find_peaks_test.py
@@ -5,38 +5,64 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
+
+import os
 import unittest
+from os.path import join
+
+import numpy as np
+import pytest
+
+from eqcorrscan.utils.findpeaks import find_peaks2_short
+from eqcorrscan.utils.findpeaks import coin_trig
 
 
-class TestPeakFinding(unittest.TestCase):
-    def test_main_find_peaks(self):
-        """Test find_peaks2_short"""
-        from eqcorrscan.utils.findpeaks import find_peaks2_short
-        import numpy as np
-        import os
-        testing_path = os.path.join(os.path.abspath(os.path.dirname(__file__)),
-                                    'test_data')
-        expected_ccc = np.load(os.path.join(testing_path, 'test_ccc.npy'))
-        peaks = find_peaks2_short(arr=expected_ccc, thresh=0.2, trig_int=10,
-                                  debug=0, starttime=False, samp_rate=200.0)
-        expected_peaks = np.load(os.path.join(testing_path, 'test_peaks.npy'))
+class TestPeakFinding:
+
+    # fixtures
+    @pytest.fixture
+    def cc_array(self):
+        """ load the test cc array case """
+        return np.load(join(pytest.test_data_path, 'test_ccc.npy'))
+
+    @pytest.fixture
+    def expected_peak_array(self):
+        """ load the peak array from running find peaks on cc_arary """
+        return np.load(join(pytest.test_data_path, 'test_peaks.npy'))
+
+    @pytest.fixture
+    def peak_array(self, cc_array):
+        """ run find_peaks2_short on cc_array and return results """
+        peaks = find_peaks2_short(arr=cc_array, thresh=0.2, trig_int=10,
+                                  debug=0, starttime=None, samp_rate=200.0)
+        return peaks
+
+    # tests
+    def test_main_find_peaks(self, peak_array, expected_peak_array):
+        """Test find_peaks2_short returns expected peaks """
+
         # Check length first as this will be a more obvious issue
-        self.assertEqual(len(peaks), len(expected_peaks),
-                         msg='Peaks are not the same length, has ccc been ' +
-                         'updated?')
-        self.assertTrue((np.array(peaks) == expected_peaks).all())
+        assert len(peak_array) == len(expected_peak_array), (
+            'Peaks are not the same length, has ccc been updated?')
+        assert (np.array(peak_array) == expected_peak_array).all()
 
-    def test_coincidence(self):
-        """Test the coincidence trigger."""
-        from eqcorrscan.utils.findpeaks import coin_trig
+
+
+
+class TestCoincidenceTrigger:
+
+    # fixtures
+    @pytest.fixture
+    def peaks(self):
+        """" create a sample peak array """
         peaks = [[(0.5, 100), (0.3, 800), (0.3, 105)],
                  [(0.4, 120), (0.7, 850)]]
+        return peaks
+
+    # tests
+    def test_coincidence(self, peaks):
+        """Test the coincidence trigger."""
+
         triggers = coin_trig(peaks, [('a', 'Z'), ('b', 'Z')], samp_rate=10,
                              moveout=3, min_trig=2, trig_int=1)
-        self.assertEqual(triggers, [(0.45, 100)])
-
-if __name__ == '__main__':
-    """
-    Run tests
-    """
-    unittest.main()
+        assert triggers, [(0.45, 100)]
